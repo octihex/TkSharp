d295214e75a8b458a78820c91cb8dadca5f4e89e
ArchLeaders <archleadership28@gmail.com>
2024-11-15 14:48:17 -0800

    Update main changelog builder

--- TkSharp.Merging/TkChangelogBuilder.cs
+++ TkSharp.Merging/TkChangelogBuilder.cs
@@ -9,6 +9,7 @@ public class TkChangelogBuilder(ITkModSource source, ITkModWriter writer, ITkRom
     private readonly ITkModSource _source = source;
     private readonly ITkModWriter _writer = writer;
     private readonly ITkRom _tk = tk;
+
     private readonly TkChangelog _changelog = new() {
         BuilderVersion = 100,
         GameVersion = tk.GameVersion
@@ -20,7 +21,7 @@ public class TkChangelogBuilder(ITkModSource source, ITkModWriter writer, ITkRom
                 file => Task.Run(() => BuildTarget(file))
             ))
             .ConfigureAwait(false);
-        
+
         return _changelog;
     }
 
@@ -29,7 +30,7 @@ public class TkChangelogBuilder(ITkModSource source, ITkModWriter writer, ITkRom
         foreach (string file in _source.Files) {
             BuildTarget(file);
         }
-        
+
         return _changelog;
     }
 
@@ -37,19 +38,21 @@ public class TkChangelogBuilder(ITkModSource source, ITkModWriter writer, ITkRom
     {
         TkPath path = TkPath.FromPath(file, _source.PathToRoot);
         string canonical = path.Canonical.ToString();
-        
+
         using Stream content = _source.OpenRead(file);
-        
+
         switch (path) {
             case { Root: "exefs", Extension: ".ips" }:
                 if (TkPatch.FromIps(content, path.Canonical[..^4].ToString()) is TkPatch patch) {
                     _changelog.PatchFiles.Add(patch);
                 }
+
                 return;
             case { Root: "exefs", Extension: ".pchtxt" }:
                 if (TkPatch.FromPchTxt(content) is TkPatch patchFromPchtxt) {
                     _changelog.PatchFiles.Add(patchFromPchtxt);
                 }
+
                 return;
             case { Root: "exefs", Canonical.Length: 7 } when path.Canonical[..6] is "subsdk":
                 _changelog.SubSdkFiles.Add(canonical);
@@ -57,39 +60,54 @@ public class TkChangelogBuilder(ITkModSource source, ITkModWriter writer, ITkRom
             case { Root: "cheats" }:
                 _changelog.CheatFiles.Add(canonical);
                 goto Copy;
+            case { Extension: ".ini" }:
+                return;
         }
-        
+
         goto Build;
-        
+
+    CopyWithMetadata:
+        AddChangelogMetadata(path, canonical, ChangelogEntryType.Copy, zsDictionaryId: -1);
+
     Copy:
         string outputFilePath = Path.Combine(path.Root.ToString(), canonical);
         // ReSharper disable once ConvertToUsingDeclaration
         using (Stream output = _writer.OpenWrite(outputFilePath)) {
+            content.Seek(0, SeekOrigin.Begin);
             content.CopyTo(output);
         }
-        
+
         return;
-        
+
     Build:
         if (GetChangelogBuilder(path) is not ITkChangelogBuilder builder) {
-            AddChangelogMetadata(path, canonical, ChangelogEntryType.Copy, zsDictionaryId: -1);
-            goto Copy;
+            goto CopyWithMetadata;
         }
-        
-        using RentedBuffer<byte> src = _tk.Zstd.Decompress(content, out int zsDictionaryId);
+
+        RentedBuffer<byte> src = _tk.Zstd.Decompress(content, out int zsDictionaryId);
 
         if (_tk.IsVanilla(path.Canonical, src.Span, path.FileVersion)) {
             return;
         }
 
-        using RentedBuffer<byte> vanilla
+        RentedBuffer<byte> vanilla
             = _tk.GetVanilla(canonical, path.Attributes);
-        
-        builder.Build(canonical, path, src.Segment, vanilla.Segment, (path, canon) => {
-            AddChangelogMetadata(path, canon, ChangelogEntryType.Changelog, zsDictionaryId);
-            string outputFile = Path.Combine(path.Root.ToString(), canon);
-            return _writer.OpenWrite(outputFile);
-        });
+
+        if (vanilla.IsEmpty) {
+            goto CopyWithMetadata;
+        }
+
+        try {
+            builder.Build(canonical, path, src.Segment, vanilla.Segment, (path, canon) => {
+                AddChangelogMetadata(path, canon, ChangelogEntryType.Changelog, zsDictionaryId);
+                string outputFile = Path.Combine(path.Root.ToString(), canon);
+                return _writer.OpenWrite(outputFile);
+            });
+        }
+        finally {
+            src.Dispose();
+            vanilla.Dispose();
+        }
     }
 
     private void AddChangelogMetadata(in TkPath path, string canonical, ChangelogEntryType type, int zsDictionaryId)
@@ -98,7 +116,7 @@ public class TkChangelogBuilder(ITkModSource source, ITkModWriter writer, ITkRom
             _changelog.MalsFiles.Add(canonical);
             return;
         }
-        
+
         _changelog.ChangelogFiles.Add(
             (canonical, new TkChangelogEntry(
                 type, path.Attributes, zsDictionaryId
