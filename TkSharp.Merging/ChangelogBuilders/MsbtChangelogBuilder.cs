718b6cc50022ab7709cddafdd297a1bfb47652dd
ArchLeaders <archleadership28@gmail.com>
2024-11-15 02:02:16 -0800

    Impl Msbt changelog builder

--- TkSharp.Merging/ChangelogBuilders/MsbtChangelogBuilder.cs
+++ TkSharp.Merging/ChangelogBuilders/MsbtChangelogBuilder.cs
@@ -1,3 +1,5 @@
+using MessageStudio.Formats.BinaryText;
+using Revrs.Extensions;
 using TkSharp.Core;
 
 namespace TkSharp.Merging.ChangelogBuilders;
@@ -6,6 +8,29 @@ public sealed class MsbtChangelogBuilder : Singleton<MsbtChangelogBuilder>, ITkC
 {
     public void Build(string canonical, in TkPath path, ArraySegment<byte> srcBuffer, ArraySegment<byte> vanillaBuffer, OpenWriteChangelog openWrite)
     {
-        throw new NotImplementedException();
+        if (srcBuffer.AsSpan().Read<ulong>() != Msbt.MAGIC) {
+            return;
+        }
+        
+        Msbt vanilla = Msbt.FromBinary(vanillaBuffer);
+
+        Msbt changelog = [];
+        Msbt src = Msbt.FromBinary(srcBuffer);
+
+        foreach ((string key, MsbtEntry entry) in src) {
+            if (!vanilla.TryGetValue(key, out MsbtEntry? vanillaEntry)) {
+                goto UpdateChangelog;
+            }
+            
+            if (entry.Text == vanillaEntry.Text && entry.Attribute == vanillaEntry.Attribute) {
+                continue;
+            }
+            
+        UpdateChangelog:
+            changelog[key] = entry;
+        }
+
+        using Stream output = openWrite(path, canonical);
+        changelog.WriteBinary(output);
     }
 }
