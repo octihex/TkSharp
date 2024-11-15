571f1573ab52234822e545b76d6cfe557134df60
ArchLeaders <archleadership28@gmail.com>
2024-11-15 14:48:05 -0800

    Create collection extensions

---
+++ TkSharp.Merging/Extensions/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+namespace TkSharp.Merging.Extensions;
+
+internal static class CollectionExtensions
+{
+    public static bool TryGetIndex<T>(this IList<T> list, T element, IEqualityComparer<T> comparer, out int index)
+    {
+        int len = list.Count;
+        for (int i = 0; i < len; i++) {
+            if (!comparer.Equals(list[i], element)) {
+                continue;
+            }
+
+            index = i;
+            return true;
+        }
+
+        index = -1;
+        return false;
+    }
+}
