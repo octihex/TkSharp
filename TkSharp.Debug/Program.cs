6f8c0ff0c9bfe58925399803c80bbbfe3286d9af
ArchLeaders <archleadership28@gmail.com>
2024-11-15 11:29:09 -0800

    Inject embedded checksums bin Setup debugging

--- TkSharp.Debug/Program.cs
+++ TkSharp.Debug/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using TkSharp.Core;
 using TkSharp.Core.IO;
+using TkSharp.Data.Embedded;
 using TkSharp.Merging;
 
 string inputModFolder = args[0];
@@ -8,15 +9,19 @@ string outputModFolder = args[1];
 
 ITkModSource source = new FolderModSource(inputModFolder);
 ITkModWriter writer = new FolderModWriter(outputModFolder);
-ITkRom rom = new ExtractedTkRom(@"F:\Games\RomFS\Totk\1.2.1");
+ITkRom rom = new ExtractedTkRom(@"F:\Games\RomFS\Totk\1.2.1",
+    TkChecksums.FromStream(TkEmbeddedDataSource.GetChecksumsBin())
+);
 
 TkChangelogBuilder builder = new(
     source, writer, rom
 );
 
 var stopwatch = Stopwatch.StartNew();
-{
-    builder.Build();
-}
+
+TkChangelog changelog = builder.Build();
+
 stopwatch.Stop();
 Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+Debugger.Break();
