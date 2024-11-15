e56e230d6a7c041e54ce8d954262c5c411f8f7b2
ArchLeaders <archleadership28@gmail.com>
2024-11-15 02:54:08 -0800

    Impl changelog builder debug script

--- TkSharp.Debug/Program.cs
+++ TkSharp.Debug/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello World!");
+﻿using System.Diagnostics;
+using TkSharp.Core;
+using TkSharp.Core.IO;
+using TkSharp.Merging;
+
+string inputModFolder = args[0];
+string outputModFolder = args[1];
+
+ITkModSource source = new FolderModSource(inputModFolder);
+ITkModWriter writer = new FolderModWriter(outputModFolder);
+ITkRom rom = new ExtractedTkRom(@"F:\Games\RomFS\Totk\1.2.1");
+
+TkChangelogBuilder builder = new(
+    source, writer, rom
+);
+
+var stopwatch = Stopwatch.StartNew();
+{
+    builder.Build();
+}
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
