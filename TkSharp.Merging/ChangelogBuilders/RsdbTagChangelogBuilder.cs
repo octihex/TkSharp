2671ba2da588d0a658d78f7b9858915582757ffb
ArchLeaders <archleadership28@gmail.com>
2024-11-16 01:27:23 -0800

    Simplify Rsdb changelog builder class names

--- TkSharp.Merging/ChangelogBuilders/RsdbTagChangelogBuilder.cs
+++ TkSharp.Merging/ChangelogBuilders/RsdbTagChangelogBuilder.cs
@@ -2,7 +2,7 @@ using TkSharp.Core;
 
 namespace TkSharp.Merging.ChangelogBuilders;
 
-public sealed class ResourceDbTagChangelogBuilder : Singleton<ResourceDbTagChangelogBuilder>, ITkChangelogBuilder
+public sealed class RsdbTagChangelogBuilder : Singleton<RsdbTagChangelogBuilder>, ITkChangelogBuilder
 {
     public void Build(string canonical, in TkPath path, ArraySegment<byte> srcBuffer, ArraySegment<byte> vanillaBuffer, OpenWriteChangelog openWrite)
     {
