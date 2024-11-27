4c8a4e096fc22a280258c334b166ffd82033d31d
ArchLeaders <archleadership28@gmail.com>
2024-11-26 19:15:48 -0800

    Impl ReadFromInput extension methods (ITkModReaderProvider)

---
+++ TkSharp.Core/TkModReader.cs
@@ -0,0 +1,32 @@
+using TkSharp.Core.Models;
+
+namespace TkSharp.Core;
+
+public static class TkModReader
+{
+    public static async Task<TkMod?> ReadFromInput(this ITkModReaderProvider readerProvider, object input, CancellationToken ct = default)
+    {
+        if (readerProvider.GetReader(input) is not ITkModReader reader) {
+            return null;
+        }
+
+        if (input is string path && File.Exists(path)) {
+            await using FileStream fs = File.OpenRead(path);
+            return await reader.ReadMod(input, fs, ct: ct)
+                .ConfigureAwait(false); 
+        }
+
+        return await reader.ReadMod(input, ct: ct)
+            .ConfigureAwait(false);
+    }
+    
+    public static async Task<TkMod?> ReadFromStream(this ITkModReaderProvider readerProvider, object input, Stream stream, CancellationToken ct = default)
+    {
+        if (readerProvider.GetReader(input) is not ITkModReader reader) {
+            return null;
+        }
+
+        return await reader.ReadMod(input, stream, ct: ct)
+            .ConfigureAwait(false);
+    }
+}
