e96a6a9fb90d79930eeb58bdb950799c75fc023c
ArchLeaders <archleadership28@gmail.com>
2024-11-26 19:14:56 -0800

    Impl extensible mod reader provider

---
+++ TkSharp/TkModReaderProvider.cs
@@ -0,0 +1,28 @@
+using TkSharp.Core;
+using TkSharp.IO.Readers;
+
+namespace TkSharp;
+
+public class TkModReaderProvider(ITkModWriterProvider tkWriterProvider, ITkRomProvider tkRomProvider)
+    : ITkModReaderProvider
+{
+    private readonly List<ITkModReader> _readers = [
+        new ArchiveModReader(tkWriterProvider, tkRomProvider),
+        new TkPackReader(tkWriterProvider),
+    ];
+
+    public void Register(ITkModReader reader)
+    {
+        _readers.Add(reader);
+    }
+
+    public ITkModReader? GetReader(object input)
+    {
+        return _readers.FirstOrDefault(reader => reader.IsKnownInput(input));
+    }
+
+    public bool CanRead(object input)
+    {
+        return _readers.Any(reader => reader.IsKnownInput(input));
+    }
+}
