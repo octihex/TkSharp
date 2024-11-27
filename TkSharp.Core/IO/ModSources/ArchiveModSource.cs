5a37281734c20ea2243b71145cac65666130efdf
ArchLeaders <archleadership28@gmail.com>
2024-11-27 11:38:09 -0800

    Always decompress zip entries into memory

--- TkSharp.Core/IO/ModSources/ArchiveModSource.cs
+++ TkSharp.Core/IO/ModSources/ArchiveModSource.cs
@@ -3,18 +3,22 @@ using SharpCompress.Archives;
 
 namespace TkSharp.Core.IO.ModSources;
 
-public sealed class ArchiveModSource(IArchive archive, IArchiveEntry rootEntry) : TkModSourceBase<IArchiveEntry>(rootEntry.Key!)
+public sealed class ArchiveModSource(IArchive archive, IArchiveEntry? rootEntry) : TkModSourceBase<IArchiveEntry>(rootEntry?.Key)
 {
     private readonly IArchive _archive = archive;
-    private readonly IArchiveEntry _rootEntry = rootEntry;
+    private readonly IArchiveEntry? _rootEntry = rootEntry;
 
     protected override IEnumerable<IArchiveEntry> Files => _archive.Entries
-        .Where(entry => !entry.IsDirectory && entry.Key?.StartsWith(_rootEntry.Key!) is true);
+        .Where(entry => !entry.IsDirectory && (_rootEntry is null || entry.Key?.StartsWith(_rootEntry.Key!) is true));
 
     protected override Stream OpenRead(IArchiveEntry input)
     {
         lock (_archive) {
-            return input.OpenEntryStream();
+            MemoryStream copy = new();
+            using Stream archive = input.OpenEntryStream();
+            archive.CopyTo(copy);
+            copy.Seek(0, SeekOrigin.Begin);
+            return copy;
         }
     }
 
