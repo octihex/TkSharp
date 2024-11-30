d85d2148d366a70cccd81f210242a41c3fe3a5e7
ArchLeaders <archleadership28@gmail.com>
2024-11-30 15:47:23 -0800

    Impl TkChangelogEntry as mutable class

--- TkSharp.Core/Models/TkChangelogEntry.cs
+++ TkSharp.Core/Models/TkChangelogEntry.cs
@@ -1,10 +1,26 @@
+using System.Text.Json.Serialization;
+
 namespace TkSharp.Core.Models;
 
-public record TkChangelogEntry(
-    string Canonical,
-    ChangelogEntryType Type,
-    TkFileAttributes Attributes,
-    int ZsDictionaryId);
+[method: JsonConstructor]
+public class TkChangelogEntry(string canonical, ChangelogEntryType type, TkFileAttributes attributes, int zsDictionaryId)
+{
+    public string Canonical { get; set; } = canonical;
+
+    public ChangelogEntryType Type { get; init; } = type;
+
+    public TkFileAttributes Attributes { get; init; } = attributes;
+
+    public int ZsDictionaryId { get; init; } = zsDictionaryId;
+
+    public void Deconstruct(out string canonical, out ChangelogEntryType type, out TkFileAttributes attributes, out int zsDictionaryId)
+    {
+        canonical = Canonical;
+        type = Type;
+        attributes = Attributes;
+        zsDictionaryId = ZsDictionaryId;
+    }
+}
 
 public enum ChangelogEntryType
 {
