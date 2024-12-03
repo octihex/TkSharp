870ae9738673c64346c669d35479596b68a2d79a
ArchLeaders <archleadership28@gmail.com>
2024-12-03 02:52:56 -0800

    Impl GDL metadata writer (mostly) I tried my best

---
+++ TkSharp.Merging/Mergers/GameData/SaveDataWriter.cs
@@ -0,0 +1,40 @@
+namespace TkSharp.Merging.Mergers.GameData;
+
+public static class SaveDataWriter
+{
+    public static readonly string[] ValidTables = [
+        "Bool",
+        "BoolArray",
+        "Int",
+        "IntArray",
+        "Float",
+        "FloatArray",
+        "Enum",
+        "EnumArray",
+        "Vector2",
+        "Vector2Array",
+        "Vector3",
+        "Vector3Array",
+        "String16",
+        "String16Array",
+        "String32",
+        "String32Array",
+        "String64",
+        "String64Array",
+        "Binary",
+        "BinaryArray",
+        "UInt",
+        "UIntArray",
+        "Int64",
+        "Int64Array",
+        "UInt64",
+        "UInt64Array",
+        "WString16",
+        "WString16Array",
+        "WString32",
+        "WString32Array",
+        "WString64",
+        "WString64Array",
+        "Bool64bitKey"
+    ];
+}
