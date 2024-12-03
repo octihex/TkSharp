65a3a55366490180de4b1cb4a945865b19ff21b6
ArchLeaders <archleadership28@gmail.com>
2024-12-02 22:04:57 -0800

    Impl GDL data merger

--- TkSharp.Merging/Mergers/GameDataMerger.cs
+++ TkSharp.Merging/Mergers/GameDataMerger.cs
@@ -1,19 +1,120 @@
+using BymlLibrary;
+using BymlLibrary.Nodes.Containers;
+using Revrs;
+using TkSharp.Core;
 using TkSharp.Core.IO.Buffers;
 using TkSharp.Core.Models;
+using TkSharp.Merging.Mergers.BinaryYaml;
 
 namespace TkSharp.Merging.Mergers;
 
-public sealed class GameDataMerger : Singleton<GameDataMerger>, ITkMerger
+public sealed class GameDataMerger(TkZstd zs) : ITkMerger
 {
+    private readonly TkZstd _zs = zs;
+
     public void Merge(TkChangelogEntry entry, RentedBuffers<byte> inputs, ArraySegment<byte> vanillaData, Stream output)
     {
-        return;
-        throw new NotImplementedException();
+        Byml merged = Byml.FromBinary(vanillaData, out Endianness endianness, out ushort version);
+        BymlMergeTracking tracking = new();
+
+        foreach (RentedBuffers<byte>.Entry input in inputs) {
+            BymlMap changelog = Byml.FromBinary(input.Span)
+                .GetMap();
+            MergeEntry(merged.GetMap()["Data"].GetMap(), changelog, tracking);
+        }
+        
+        tracking.Apply();
+
+        BymlMerger.WriteOutput(entry, merged, endianness, version, output, _zs);
+    }
+
+    public void Merge(TkChangelogEntry entry, IEnumerable<ArraySegment<byte>> inputs, ArraySegment<byte> vanillaData, Stream output)
+    {
+        Byml merged = Byml.FromBinary(vanillaData, out Endianness endianness, out ushort version);
+        BymlMergeTracking tracking = new();
+
+        foreach (ArraySegment<byte> input in inputs) {
+            BymlMap changelog = Byml.FromBinary(input)
+                .GetMap();
+            MergeEntry(merged.GetMap()["Data"].GetMap(), changelog, tracking);
+        }
+        
+        tracking.Apply();
+
+        BymlMerger.WriteOutput(entry, merged, endianness, version, output, _zs);
     }
 
     public void MergeSingle(TkChangelogEntry entry, ArraySegment<byte> input, ArraySegment<byte> @base, Stream output)
     {
-        return;
-        throw new NotImplementedException();
+        Byml merged = Byml.FromBinary(@base, out Endianness endianness, out ushort version);
+        BymlMergeTracking tracking = new();
+
+        BymlMap changelog = Byml.FromBinary(input)
+            .GetMap();
+        MergeEntry(merged.GetMap()["Data"].GetMap(), changelog, tracking);
+        
+        tracking.Apply();
+
+        BymlMerger.WriteOutput(entry, merged, endianness, version, output, _zs);
+    }
+
+    private static void MergeEntry(BymlMap merged, BymlMap changelog, BymlMergeTracking tracking)
+    {
+        foreach ((string key, Byml entry) in changelog) {
+            BymlArray @base = merged[key].GetArray();
+            switch (entry.Value) {
+                case IDictionary<uint, Byml> hashMap32:
+                    MergeHashMap32(@base, hashMap32, tracking);
+                    break;
+                case IDictionary<ulong, Byml> hashMap64:
+                    MergeHashMap64(@base, hashMap64, tracking);
+                    break;
+                default:
+                    throw new NotSupportedException(
+                        $"Invalid GameDataList changelog array map type: '{entry.Type}'");
+            }
+        }
+    }
+
+    private static void MergeHashMap32(BymlArray @base, IDictionary<uint, Byml> changelog, BymlMergeTracking tracking)
+    {
+        foreach (Byml baseEntry in @base) {
+            if (baseEntry.Value is not IDictionary<string, Byml> map ||
+                !map.TryGetValue("Hash", out Byml? hashEntry) || hashEntry.Value is not uint hash) {
+                // Invalid vanilla GDL entry
+                continue;
+            }
+
+            if (!changelog.Remove(hash, out Byml? changelogEntry)) {
+                continue;
+            }
+            
+            BymlMerger.Merge(baseEntry, changelogEntry, tracking);
+        }
+
+        foreach ((uint _, Byml entry) in changelog) {
+            @base.Add(entry);
+        }
+    }
+
+    private static void MergeHashMap64(BymlArray @base, IDictionary<ulong, Byml> changelog, BymlMergeTracking tracking)
+    {
+        foreach (Byml baseEntry in @base) {
+            if (baseEntry.Value is not IDictionary<string, Byml> map ||
+                !map.TryGetValue("Hash", out Byml? hashEntry) || hashEntry.Value is not ulong hash) {
+                // Invalid vanilla GDL entry
+                continue;
+            }
+
+            if (!changelog.Remove(hash, out Byml? changelogEntry)) {
+                continue;
+            }
+            
+            BymlMerger.Merge(baseEntry, changelogEntry, tracking);
+        }
+
+        foreach ((ulong _, Byml entry) in changelog) {
+            @base.Add(entry);
+        }
     }
 }
