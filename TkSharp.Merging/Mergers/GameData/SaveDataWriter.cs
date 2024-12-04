a9f7affd6235784a7867b00e35ba00fc44da5ce4
ArchLeaders <archleadership28@gmail.com>
2024-12-03 19:42:21 -0800

    Update/rewrite GDL metadata writer

--- TkSharp.Merging/Mergers/GameData/SaveDataWriter.cs
+++ TkSharp.Merging/Mergers/GameData/SaveDataWriter.cs
@@ -1,40 +1,189 @@
+using BymlLibrary;
+using BymlLibrary.Nodes.Containers;
+
 namespace TkSharp.Merging.Mergers.GameData;
 
 public static class SaveDataWriter
 {
+    private const int DEFAULT_SIZE = 0x130;
+
     public static readonly string[] ValidTables = [
+        "Binary",
+        "BinaryArray",
         "Bool",
+        "Bool64bitKey",
         "BoolArray",
-        "Int",
-        "IntArray",
-        "Float",
-        "FloatArray",
         "Enum",
         "EnumArray",
-        "Vector2",
-        "Vector2Array",
-        "Vector3",
-        "Vector3Array",
+        "Float",
+        "FloatArray",
+        "Int",
+        "Int64",
+        "Int64Array",
+        "IntArray",
         "String16",
         "String16Array",
         "String32",
         "String32Array",
         "String64",
         "String64Array",
-        "Binary",
-        "BinaryArray",
         "UInt",
-        "UIntArray",
-        "Int64",
-        "Int64Array",
         "UInt64",
         "UInt64Array",
+        "UIntArray",
+        "Vector2",
+        "Vector2Array",
+        "Vector3",
+        "Vector3Array",
         "WString16",
         "WString16Array",
         "WString32",
         "WString32Array",
         "WString64",
-        "WString64Array",
-        "Bool64bitKey"
+        "WString64Array"
     ];
+
+    public static void CalculateMetadata(BymlMap metaData, BymlMap tables)
+    {   
+        int[] saveDataOffsets = new int[7];
+        int[] saveDataSizes = new int[7];
+
+        Metadata metaDataStore = new(saveDataOffsets, saveDataSizes);
+
+        BymlArray saveDirectories = metaData["SaveDirectory"]
+            .GetArray();
+
+        foreach (string tableName in ValidTables) {
+            CalculateTableMetadata(ref metaDataStore,
+                tableName, tables.GetValueOrDefault(tableName)?.Value as BymlArray,
+                saveDirectories);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            ref int saveDataOffset = ref saveDataOffsets[i];
+            if (saveDataOffset > 0) saveDataOffset += DEFAULT_SIZE;
+
+            ref int saveDataSize = ref saveDataSizes[i];
+            if (saveDataSize > 0) saveDataSize += DEFAULT_SIZE;
+        }
+
+        Console.WriteLine(metaDataStore.AllDataSaveOffset);
+        Console.WriteLine(metaDataStore.AllDataSaveSize);
+
+        metaData["AllDataSaveOffset"] = metaDataStore.AllDataSaveOffset;
+        metaData["AllDataSaveSize"] = metaDataStore.AllDataSaveSize;
+        metaData["SaveDataOffsetPos"] = new BymlArray(saveDataOffsets.Select(value => (Byml)value));
+        metaData["SaveDataSize"] = new BymlArray(saveDataSizes.Select(value => (Byml)value));
+    }
+
+    private static void CalculateTableMetadata(ref Metadata metadata, string tableName, BymlArray? table, BymlArray saveDirectories)
+    {
+        if (table is null) {
+            return;
+        }
+        
+        bool isBool64BitKey = tableName is "Bool64bitKey";
+        bool isArrayTable = tableName.Length > 5 && tableName.AsSpan()[^5..] is "Array";
+
+        foreach (BymlMap entry in table.Select(x => x.GetMap())) {
+            if (!entry.TryGetValue("SaveFileIndex", out Byml? saveFileIndexEntry) || saveFileIndexEntry.Value is not int saveFileIndex) {
+                continue;
+            }
+
+            if (saveFileIndex is -1 || saveDirectories[saveFileIndex].Value is "") {
+                if (isBool64BitKey) {
+                    continue;
+                }
+                
+                metadata.AllDataSaveOffset += 8;
+                metadata.AllDataSaveSize += CalculateEntrySize(tableName, entry, isArrayTable);
+                continue;
+            }
+
+            ref int offset = ref metadata.SaveDataOffsets[saveFileIndex];
+            ref int size = ref metadata.SaveDataSizes[saveFileIndex];
+
+            if (!isBool64BitKey) {
+                offset += 8;
+                metadata.AllDataSaveOffset += 8;
+                goto CalculateEntry;
+            }
+
+            ref bool hasSizeBeenUpdated = ref metadata.SaveDataSizesHasKey[saveFileIndex];
+            if (!hasSizeBeenUpdated) {
+                size += 8;
+                metadata.AllDataSaveSize += 8;
+                hasSizeBeenUpdated = true;
+            }
+
+        CalculateEntry:
+            int entrySize = CalculateEntrySize(tableName, entry, isArrayTable);
+            size += entrySize;
+            metadata.AllDataSaveSize += entrySize;
+        }
+    }
+
+    private static int CalculateEntrySize(string tableName, BymlMap entry, bool isArrayTable)
+    {
+        (int entryCount, int entrySize, Range tableTypeMask) = isArrayTable switch {
+            true => (GetArrayCount(tableName, entry), 0xC, ..^5),
+            false => (1, 0x8, ..)
+        };
+
+        return GetEntrySize(tableName, tableName.AsSpan()[tableTypeMask],
+            entrySize, entryCount, entry);
+    }
+
+    private static int GetArrayCount(string tableName, BymlMap entry)
+    {
+        if (entry.TryGetValue("ArraySize", out Byml? entryArraySize)) {
+            return (int)entryArraySize.GetUInt32();
+        }
+
+        if (entry.TryGetValue("Size", out Byml? entrySize)) {
+            return (int)entrySize.GetUInt32();
+        }
+
+        if (entry.TryGetValue("DefaultValue", out Byml? defaultValue) && defaultValue.Value is BymlArray defaultValueArray) {
+            return defaultValueArray.Count;
+        }
+
+        throw new InvalidDataException(
+            $"The length of an array entry in '{tableName}' could not be determined.");
+    }
+
+    private static int GetEntrySize(string tableName, ReadOnlySpan<char> tableType, int size, int count, BymlMap entry)
+    {
+        return tableName switch {
+            "BoolArray" => size + (int)Math.Ceiling((Math.Ceiling(count / 8d) is > 3 and var value ? value : 4) / 4) * 4,
+            "IntArray" or "FloatArray" or "UIntArray" or "EnumArray" => size + count * 4,
+            "Binary" or "BinaryArray" => entry.GetValueOrDefault("DefaultValue")?.Value switch {
+                uint defaultValue => size + count * 4
+                                          + count * (int)defaultValue,
+                _ => size + count * 4
+            },
+            _ => size + count * tableType switch {
+                "UInt64" or "Int64" or "Vector2" => 8,
+                "Vector3" => 12,
+                "String16" => 16,
+                "String32" or "WString16" => 32,
+                "String64" or "WString32" => 64,
+                "WString64" => 128,
+                _ => 0
+            }
+        };
+    }
+
+    private ref struct Metadata(int[] saveDataOffsets, int[] saveDataSizes)
+    {
+        public readonly Span<int> SaveDataOffsets = saveDataOffsets;
+
+        public readonly Span<int> SaveDataSizes = saveDataSizes;
+
+        public readonly Span<bool> SaveDataSizesHasKey = new bool[7];
+
+        public int AllDataSaveOffset = DEFAULT_SIZE;
+
+        public int AllDataSaveSize = DEFAULT_SIZE;
+    }
 }
--- TkSharp.Merging/Mergers/GameDataMerger.cs
+++ TkSharp.Merging/Mergers/GameDataMerger.cs
@@ -30,7 +30,7 @@ public sealed class GameDataMerger(TkZstd zs) : ITkMerger
 
         tracking.Apply();
 
-        GenerateMetadata(root["MetaData"].GetMap(), baseData);
+        SaveDataWriter.CalculateMetadata(root["MetaData"].GetMap(), baseData);
 
         BymlMerger.WriteOutput(entry, merged, endianness, version, output, _zs);
     }
@@ -50,7 +50,7 @@ public sealed class GameDataMerger(TkZstd zs) : ITkMerger
 
         tracking.Apply();
 
-        GenerateMetadata(root["MetaData"].GetMap(), baseData);
+        SaveDataWriter.CalculateMetadata(root["MetaData"].GetMap(), baseData);
 
         BymlMerger.WriteOutput(entry, merged, endianness, version, output, _zs);
     }
@@ -68,7 +68,7 @@ public sealed class GameDataMerger(TkZstd zs) : ITkMerger
 
         tracking.Apply();
 
-        GenerateMetadata(root["MetaData"].GetMap(), baseData);
+        SaveDataWriter.CalculateMetadata(root["MetaData"].GetMap(), baseData);
 
         BymlMerger.WriteOutput(entry, merged, endianness, version, output, _zs);
     }
@@ -131,203 +131,5 @@ public sealed class GameDataMerger(TkZstd zs) : ITkMerger
         foreach ((ulong _, Byml entry) in changelog) {
             @base.Add(entry);
         }
-    }
-
-    private static void GenerateMetadata(BymlMap metadata, BymlMap tables)
-    {
-        BymlArray saveDataOffsets = new(7);
-        BymlArray saveDataSizes = new(7);
-
-        BymlArray saveDirectories = metadata["SaveDirectory"]
-            .GetArray();
-
-        for (int i = 0; i < 7; i++) {
-            (int offset, int size) = GetSaveDataOffset(i, saveDirectories, tables);
-            saveDataOffsets.Add(offset);
-            saveDataSizes.Add(size);
-        }
-
-        int allDataSaveOffset = 0;
-        int allDataSaveSize = 0;
-
-        // foreach ((string tableNameStr, Byml table) in tables) {
-        //     BymlArray tableEntries = table.GetArray();
-        //     ReadOnlySpan<char> tableName = tableNameStr.AsSpan();
-        //
-        //     switch (tableName) {
-        //         case "Struct" or "BoolExp":
-        //             continue;
-        //         case "Bool64bitKey":
-        //             allDataSaveOffset += 8;
-        //             allDataSaveSize += 8;
-        //             CalculateBool64Table(ref allDataSaveOffset, ref allDataSaveSize, -1, tableEntries);
-        //             continue;
-        //     }
-        //
-        //     if (tableName.Length > 5 && tableName[^5..] is "Array") {
-        //         CalculateArrayTable(ref allDataSaveOffset, ref allDataSaveSize, -1, tableName, tableEntries);
-        //         continue;
-        //     }
-        //     
-        //     CalculateTable(ref allDataSaveOffset, ref allDataSaveSize, -1, tableName, tableEntries);
-        // }
-
-        metadata["AllDataSaveOffset"] = allDataSaveOffset;
-        metadata["AllDataSaveSize"] = allDataSaveSize;
-        metadata["SaveDataOffsetPos"] = saveDataOffsets;
-        metadata["SaveDataSize"] = saveDataSizes;
-    }
-
-    private static (int Offset, int Size) GetSaveDataOffset(int index, in BymlArray saveDirectories, in BymlMap tables)
-    {
-        if (index != -1 && (saveDirectories[index].Value is not string saveDirectory || string.IsNullOrWhiteSpace(saveDirectory))) {
-            return (Offset: 0, Size: 0);
-        }
-
-        int offset = 0x20;
-        int size = 0x20;
-
-        foreach (string tableNameStr in SaveDataWriter.ValidTables) {
-            ReadOnlySpan<char> tableName = tableNameStr.AsSpan();
-
-            offset += 8;
-            size += 8;
-
-            if (tableName is "Bool64bitKey") {
-                offset += 8;
-                size += 8;
-                CalculateBool64Table(ref size, index, tables[tableNameStr].GetArray());
-                continue;
-            }
-
-            if (!tables.TryGetValue(tableNameStr, out Byml? tableEntry) || tableEntry.Value is not BymlArray tableEntries) {
-                continue;
-            }
-
-            if (tableName.Length > 5 && tableName[^5..] is "Array") {
-                CalculateArrayTable(ref offset, ref size, index, tableName, tableEntries);
-                continue;
-            }
-
-            CalculateTable(ref offset, ref size, index, tableName, tableEntries);
-        }
-
-        return (offset, size);
-    }
-
-    private static void CalculateBool64Table(ref int size, int index, BymlArray table)
-    {
-        bool hasKeys = false;
-
-        foreach (Byml entry in table) {
-            if (!IsMatchingSaveFileIndex(entry, index, out IDictionary<string, Byml>? entryMap)) {
-                continue;
-            }
-
-            hasKeys = true;
-            size += SaveDataGetSize("Bool64bitKey", entryMap);
-        }
-
-        if (hasKeys) {
-            size += 8;
-        }
-    }
-
-    private static void CalculateArrayTable(ref int offset, ref int size, int index, ReadOnlySpan<char> tableName, BymlArray table)
-    {
-        foreach (Byml entry in table) {
-            if (!IsMatchingSaveFileIndex(entry, index, out IDictionary<string, Byml>? entryMap)) {
-                continue;
-            }
-
-            offset += 8;
-            size += SaveDataGetArraySize(tableName, entryMap);
-        }
-    }
-
-    private static void CalculateTable(ref int offset, ref int size, int index, ReadOnlySpan<char> tableName, BymlArray table)
-    {
-        foreach (Byml entry in table) {
-            if (!IsMatchingSaveFileIndex(entry, index, out IDictionary<string, Byml>? entryMap)) {
-                continue;
-            }
-
-            offset += 8;
-            size += SaveDataGetSize(tableName, entryMap);
-        }
-    }
-
-    private static int SaveDataGetArraySize(ReadOnlySpan<char> tableName, IDictionary<string, Byml> entry)
-    {
-        int count;
-
-        if (entry.TryGetValue("ArraySize", out Byml? entryArraySize)) {
-            count = (int)entryArraySize.GetUInt32();
-            goto CalculateSize;
-        }
-
-        if (entry.TryGetValue("Size", out Byml? entrySize)) {
-            count = (int)entrySize.GetUInt32();
-            goto CalculateSize;
-        }
-
-        if (entry.TryGetValue("DefaultValue", out Byml? defaultValue) && defaultValue.Value is BymlArray defaultValueArray) {
-            count = defaultValueArray.Count;
-            goto CalculateSize;
-        }
-
-        throw new InvalidDataException(
-            $"The length of an array entry in '{tableName}' could not be determined.");
-
-    CalculateSize:
-        return SaveDataGetSize(
-            size: 0xC, count, tableName, tableName[..^5], entry);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static int SaveDataGetSize(ReadOnlySpan<char> tableName, IDictionary<string, Byml> entry)
-    {
-        return SaveDataGetSize(
-            size: 0x8, count: 1, tableName, tableName, entry);
-    }
-
-    private static int SaveDataGetSize(int size, int count, ReadOnlySpan<char> tableName, ReadOnlySpan<char> tableType, IDictionary<string, Byml> entry)
-    {
-        switch (tableName) {
-            case "BoolArray": {
-                double div = Math.Ceiling(count / 8d);
-                return size + (int)Math.Ceiling((div < 4 ? 4 : div) / 4) * 4;
-            }
-            case "IntArray" or "FloatArray" or "UIntArray" or "EnumArray":
-                return size + count * 4;
-            case "Binary" or "BinaryArray": {
-                if (entry.TryGetValue("DefaultValue", out Byml? entryDefaultValue) && entryDefaultValue.Value is uint defaultValue) {
-                    return size + count * 4
-                                + count * (int)defaultValue;
-                }
-
-                return count * 4;
-            }
-        }
-
-        return size + count * tableType switch {
-            "Int64" or "Vector2" => 8,
-            "Vector3" => 12,
-            "String16" => 16,
-            "String32" or "WString16" => 32,
-            "String64" or "WString32" => 64,
-            "WString64" => 128,
-            _ => 0
-        };
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static bool IsMatchingSaveFileIndex(Byml entry, int index, [MaybeNullWhen(false)] out IDictionary<string, Byml> entryMap)
-    {
-        entryMap = entry.Value as IDictionary<string, Byml>;
-        return entryMap is not null
-               && entryMap.TryGetValue("SaveFileIndex", out Byml? entrySaveFileIndex)
-               && entrySaveFileIndex.Value is int saveFileIndex
-               && saveFileIndex == index;
     }
 }
