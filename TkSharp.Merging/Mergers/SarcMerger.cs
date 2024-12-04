ccccf71cfcfce8fca88daa24fd8044e02e121539
ArchLeaders <archleadership28@gmail.com>
2024-12-04 10:56:28 -0800

    Update sarc merger Impl rstb collection (nested)

--- TkSharp.Merging/Mergers/SarcMerger.cs
+++ TkSharp.Merging/Mergers/SarcMerger.cs
@@ -1,18 +1,18 @@
-using CommunityToolkit.HighPerformance.Buffers;
+using Revrs.Extensions;
 using SarcLibrary;
 using TkSharp.Core;
 using TkSharp.Core.IO.Buffers;
 using TkSharp.Core.Models;
 using TkSharp.Merging.ResourceSizeTable;
-using NestedMergeTarget = (TkSharp.Merging.ITkMerger Merger, System.ArraySegment<byte> VanillaData, System.Collections.Generic.List<System.ArraySegment<byte>> Targets); 
 
 namespace TkSharp.Merging.Mergers;
 
-public sealed class SarcMerger(TkMerger masterMerger, TkResourceSizeCollector resourceSizeCollector, TkZstd zs) : ITkMerger
+public sealed class SarcMerger(TkMerger masterMerger, TkResourceSizeCollector resourceSizeCollector) : ITkMerger
 {
+    private const ulong DELETED_MARK = 0x44564D5243534B54;
     private readonly TkMerger _masterMerger = masterMerger;
     private readonly TkResourceSizeCollector _resourceSizeCollector = resourceSizeCollector;
-    private readonly TkZstd _zs = zs;
+
     private readonly TkChangelogEntry _fakeEntry = new(
         string.Empty,
         ChangelogEntryType.Changelog,
@@ -22,109 +22,106 @@ public sealed class SarcMerger(TkMerger masterMerger, TkResourceSizeCollector re
     public void Merge(TkChangelogEntry entry, RentedBuffers<byte> inputs, ArraySegment<byte> vanillaData, Stream output)
     {
         Sarc merged = Sarc.FromBinary(vanillaData);
-        Dictionary<string, NestedMergeTarget> mergeTargets = [];
-        
-        foreach (RentedBuffers<byte>.Entry input in inputs) {
-            Sarc changelog = Sarc.FromBinary(input.Segment);
-            MergeEntry(merged, changelog, mergeTargets);
-        }
+        var changelogs = new Sarc[inputs.Count];
 
-        MergeNestedTargets(merged, mergeTargets);
+        for (int i = 0; i < inputs.Count; i++) {
+            RentedBuffers<byte>.Entry input = inputs[i];
+            changelogs[i] = Sarc.FromBinary(input.Segment);
+        }
         
-        WriteOutput(entry, merged, output);
+        MergeMany(merged, changelogs);
+        merged.Write(output);
     }
 
     public void Merge(TkChangelogEntry entry, IEnumerable<ArraySegment<byte>> inputs, ArraySegment<byte> vanillaData, Stream output)
     {
         Sarc merged = Sarc.FromBinary(vanillaData);
-        Dictionary<string, NestedMergeTarget> mergeTargets = [];
-        
-        foreach (ArraySegment<byte> input in inputs) {
-            Sarc changelog = Sarc.FromBinary(input);
-            MergeEntry(merged, changelog, mergeTargets);
-        }
-        
-        MergeNestedTargets(merged, mergeTargets);
-        
-        WriteOutput(entry, merged, output);
+        MergeMany(merged, inputs.Select(Sarc.FromBinary));
+        merged.Write(output);
     }
 
     public void MergeSingle(TkChangelogEntry entry, ArraySegment<byte> input, ArraySegment<byte> @base, Stream output)
     {
         Sarc merged = Sarc.FromBinary(@base);
-        Sarc changelog = Sarc.FromBinary(input);  
-        MergeEntry(merged, changelog, mergeTargets: null);
-        WriteOutput(entry, merged, output);
+        Sarc changelog = Sarc.FromBinary(input);
+        MergeSingle(merged, changelog);
+        merged.Write(output);
     }
 
-    private void MergeNestedTargets(Sarc merged, Dictionary<string, NestedMergeTarget> mergeTargets)
+    private void MergeMany(Sarc merged, IEnumerable<Sarc> changelogs)
     {
-        foreach ((string name, NestedMergeTarget target) in mergeTargets) {
-            using Stream nestedOutput = merged.OpenWrite(name);
-            _fakeEntry.Canonical = name;
-            
-            switch (target.Targets.Count) {
-                case >1:
-                    target.Merger.Merge(_fakeEntry, target.Targets, target.VanillaData, nestedOutput);
-                    break;
-                default:
-                    target.Merger.MergeSingle(_fakeEntry, target.Targets[0], target.VanillaData, nestedOutput);
-                    break;
-            }
-        }
-    }
-
-    private void MergeEntry(in Sarc merged, Sarc changelog, Dictionary<string, NestedMergeTarget>? mergeTargets)
-    {
-        foreach ((string name, ArraySegment<byte> data) in changelog) {
-            if (_masterMerger.GetMerger(name) is not ITkMerger merger) {
-                merged[name] = data;
-                continue;
-            }
+        IEnumerable<(string Name, ArraySegment<byte>[] Buffers)> groups = changelogs
+            .SelectMany(x => x)
+            .GroupBy(x => x.Key, x => x.Value)
+            .Select(x => (x.Key, x.ToArray()));
+        
+        foreach ((string name, ArraySegment<byte>[] buffers) in groups) {
+            ArraySegment<byte> last = buffers[^1];
             
             if (!merged.TryGetValue(name, out ArraySegment<byte> vanillaData)) {
+                merged[name] = last;
+                CalculateRstb(name, last, isFileVanilla: false);
+                continue;
+            }
+
+            if (_masterMerger.GetMerger(name) is not ITkMerger merger) {
+                merged[name] = last;
+                CalculateRstb(name, last, isFileVanilla: false);
+                continue;
+            }
+
+            if (last.Count == 8 && last.AsSpan().Read<ulong>() == DELETED_MARK) {
+                merged.Remove(name);
+                continue;
+            }
+            
+            // TODO: This is a bit sketchy
+            _fakeEntry.Canonical = name;
+            
+            if (buffers.Length == 1) {
+                using (Stream output = merged.OpenWrite(name)) {
+                    merger.MergeSingle(_fakeEntry, buffers[0], vanillaData, output);
+                }
+
+                goto CalculateMergedRstb;
+            }
+            
+            using (Stream output = merged.OpenWrite(name)) {
+                merger.Merge(_fakeEntry, buffers, vanillaData, output);
+            }
+            
+        CalculateMergedRstb:
+            CalculateRstb(name, merged[name], isFileVanilla: false);
+        }
+    }
+
+    private void MergeSingle(in Sarc merged, Sarc changelog)
+    {
+        foreach ((string name, ArraySegment<byte> data) in changelog) {
+            if (!merged.TryGetValue(name, out ArraySegment<byte> vanillaData)) {
                 merged[name] = data;
+                CalculateRstb(name, data, isFileVanilla: false);
+                continue;
+            }
+
+            if (_masterMerger.GetMerger(name) is not ITkMerger merger) {
+                merged[name] = data;
+                CalculateRstb(name, data, isFileVanilla: false);
                 continue;
             }
 
             _fakeEntry.Canonical = name;
 
-            if (mergeTargets is null) {
-                using Stream output = merged.OpenWrite(name);
+            using (Stream output = merged.OpenWrite(name)) {
                 merger.MergeSingle(_fakeEntry, data, vanillaData, output);
-                continue;
             }
 
-            if (!mergeTargets.TryGetValue(name, out NestedMergeTarget target)) {
-                mergeTargets[name] = (
-                    merger,
-                    vanillaData,
-                    Targets: [data]
-                );
-                
-                continue;
-            }
-            
-            target.Targets.Add(data);
+            CalculateRstb(name, merged[name], isFileVanilla: true);
         }
     }
 
-    private void WriteOutput(TkChangelogEntry entry, Sarc merged, Stream output)
+    private void CalculateRstb(string canonical, Span<byte> data, bool isFileVanilla)
     {
-        using MemoryStream ms = new();
-        merged.Write(ms);
-
-        if (!ms.TryGetBuffer(out ArraySegment<byte> buffer)) {
-            buffer = ms.ToArray();
-        }
-
-        if (entry.Attributes.HasFlag(TkFileAttributes.HasZsExtension)) {
-            using SpanOwner<byte> compressed = SpanOwner<byte>.Allocate(buffer.Count);
-            int compressedSize = _zs.Compress(buffer, compressed.Span, entry.ZsDictionaryId);
-            output.Write(compressed.Span[..compressedSize]);
-            return;
-        }
-        
-        output.Write(buffer);
+        _resourceSizeCollector.Collect(data.Length, canonical, isFileVanilla, data);
     }
 }
