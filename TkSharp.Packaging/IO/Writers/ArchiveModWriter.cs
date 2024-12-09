9d982f70d5a8661225619c7d20663ecf9a96f059
ArchLeaders <archleadership28@gmail.com>
2024-12-08 20:35:19 -0800

    Improve package speeds by using .net zip library

--- TkSharp.Packaging/IO/Writers/ArchiveModWriter.cs
+++ TkSharp.Packaging/IO/Writers/ArchiveModWriter.cs
@@ -1,16 +1,26 @@
-using SharpCompress.Common;
-using SharpCompress.Writers.Zip;
+using System.IO.Compression;
 using TkSharp.Core;
+using TkSharp.Core.Extensions;
 
 namespace TkSharp.Packaging.IO.Writers;
 
-public sealed class ArchiveModWriter(Stream output) : ITkModWriter
+public sealed class ArchiveModWriter : ITkModWriter
 {
-    private static readonly ZipWriterEntryOptions _defaultEntryOptions = new();
-    private readonly ZipWriter _writer = new(output, new ZipWriterOptions(CompressionType.BZip2));
+    private readonly Dictionary<string, MemoryStream> _entries = [];
     
     public Stream OpenWrite(string filePath)
     {
-        return _writer.WriteToStream(filePath, _defaultEntryOptions);
+        return _entries[filePath] = new MemoryStream();
+    }
+
+    public void Compile(Stream output)
+    {
+        using ZipArchive writer = new(output, ZipArchiveMode.Create);
+
+        foreach ((string fileName, MemoryStream ms) in _entries.OrderBy(entry => entry.Key)) {
+            using Stream entry = writer.CreateEntry(fileName).Open();
+            entry.Write(ms.GetSpan());
+            ms.Dispose();
+        }
     }
 }
