bc872195ef94cab118cd8d64cee1fdf9cf03b535
ArchLeaders <archleadership28@gmail.com>
2024-12-08 20:36:41 -0800

    Update packaging page

--- Tools/TkSharp.DevTools/ViewModels/PackagingPageViewModel.cs
+++ Tools/TkSharp.DevTools/ViewModels/PackagingPageViewModel.cs
@@ -1,10 +1,9 @@
-using Avalonia;
-using Avalonia.Platform;
 using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 using TkSharp.Core;
+using TkSharp.Core.Models;
 using TkSharp.Packaging;
 
 namespace TkSharp.DevTools.ViewModels;
@@ -35,15 +34,27 @@ public partial class PackagingPageViewModel : ObservableObject
     [RelayCommand]
     private async Task OpenProject()
     {
-        if (await App.Storage.OpenFolderPickerAsync(new FolderPickerOpenOptions { Title = "Open a TotK mod project folder." }) is not [IStorageFolder folder]) {
-            TkLog.Instance.LogInformation("Folder picker operation returned an invalid result or was cancelled.");
+        FilePickerOpenOptions filePickerOpenOptions = new() {
+            Title = "Open a TotK mod project.",
+            AllowMultiple = false,
+            FileTypeFilter = [
+                new FilePickerFileType("TotK Project") {
+                    Patterns = [
+                        "*.tkproj"
+                    ]
+                }
+            ]
+        };
+        
+        if (await App.Storage.OpenFilePickerAsync(filePickerOpenOptions) is not [IStorageFile file]) {
+            TkLog.Instance.LogInformation("File picker operation returned an invalid result or was cancelled.");
             return;
         }
 
-        if (folder.TryGetLocalPath() is not string localFolderPath) {
+        if (file.TryGetLocalPath() is not string localFilePath || Path.GetDirectoryName(localFilePath) is not string localFolderPath) {
             TkLog.Instance.LogError(
-                "Storage folder {Folder} could not be converted into a local folder path.",
-                folder);
+                "Storage file {File} could not be converted into a local file path.",
+                file);
             return;
         }
         
@@ -51,20 +62,52 @@ public partial class PackagingPageViewModel : ObservableObject
     }
     
     [RelayCommand]
-    private static async Task Save()
+    private void Save()
     {
-        
+        Project?.Save();
+        Project = null;
     }
     
     [RelayCommand]
-    private static async Task Package()
+    private async Task Package()
     {
+        if (Project is null) {
+            return;
+        }
         
+        FilePickerSaveOptions filePickerOptions = new() {
+            Title = "Export TotK changelog package.",
+            SuggestedFileName = $"{Project.Mod.Name}.tkcl",
+            DefaultExtension = ".tkcl",
+            FileTypeChoices = [
+                new FilePickerFileType("TotK Changelog Package") {
+                    Patterns = [
+                        "*.tkcl"
+                    ]
+                }
+            ]
+        };
+        
+        if (await App.Storage.SaveFilePickerAsync(filePickerOptions) is not IStorageFile file) {
+            TkLog.Instance.LogInformation("File picker operation returned an invalid result or was cancelled.");
+            return;
+        }
+
+        await using Stream output = await file.OpenWriteAsync();
+        await Project.Package(output, TkApp.TkRomProvider.GetRom());
     }
     
     [RelayCommand]
-    private static async Task PackageAndInstall()
+    private async Task Install()
     {
-        
+        if (Project is null) {
+            return;
+        }
+
+        ITkModWriter writer = TkApp.ModManager.GetSystemWriter(new TkModContext(Project.Mod.Id));
+        await Project.Build(writer,
+            TkApp.TkRomProvider.GetRom(),
+            TkApp.ModManager.GetSystemSource(Project.Mod.Id.ToString())
+        );
     }
 }
