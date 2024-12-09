06a801aee3c0e447f4a4f73141b23545937fab0c
ArchLeaders <archleadership28@gmail.com>
2024-12-09 02:27:38 -0800

    Impl TkProject I/O

--- TkSharp.Packaging/TkProject.cs
+++ TkSharp.Packaging/TkProject.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.Extensions.Logging;
@@ -18,13 +19,15 @@ public partial class TkProject(string folderPath) : ObservableObject
     
     [ObservableProperty]
     private TkMod _mod = new();
+    
+    private readonly Dictionary<TkItem, string> _itemPathLookup = [];
 
     public async ValueTask Package(Stream output, ITkRom rom, CancellationToken ct = default)
     {
         TkLog.Instance.LogInformation("Packaging '{ModName}'", Mod.Name);
         
         ArchiveModWriter writer = new();
-        Mod.Changelog = await Build(writer, rom, ct: ct);
+        await Build(writer, rom, ct: ct);
         
         using MemoryStream contentArchiveOutput = new();
         writer.Compile(contentArchiveOutput);
@@ -34,16 +37,31 @@ public partial class TkProject(string folderPath) : ObservableObject
         TkLog.Instance.LogInformation("'{ModName}' packaging completed", Mod.Name);
     }
 
-    public async ValueTask<TkChangelog> Build(ITkModWriter writer, ITkRom rom, ITkSystemSource? systemSource = null, CancellationToken ct = default)
+    public async ValueTask Build(ITkModWriter writer, ITkRom rom, ITkSystemSource? systemSource = null, CancellationToken ct = default)
     {
-        TkLog.Instance.LogInformation("Building '{ModName}'", Mod.Name);
-        
         FolderModSource source = new(FolderPath);
+        Mod.Changelog = await Build(Mod, source, writer, rom, systemSource, ct);
+
+        foreach (TkModOption option in Mod.OptionGroups.SelectMany(group => group.Options)) {
+            if (!TryGetPath(option, out string? optionPath)) {
+                continue;
+            }
+            
+            FolderModSource optionSource = new(optionPath);
+            writer.SetRelativeFolder(option.Id.ToString());
+            option.Changelog = await Build(option, optionSource, writer, rom, ct: ct);
+        }
+    }
+
+    private static async ValueTask<TkChangelog> Build(TkStoredItem item, ITkModSource source, ITkModWriter writer, ITkRom rom, ITkSystemSource? systemSource = null, CancellationToken ct = default)
+    {
+        TkLog.Instance.LogInformation("Building '{ItemName}'", item.Name);
+        
         TkChangelogBuilder builder = new(source, writer, rom, systemSource);
         TkChangelog result = await builder.BuildAsync(ct)
             .ConfigureAwait(false);
         
-        TkLog.Instance.LogInformation("'{ModName}' build completed", Mod.Name);
+        TkLog.Instance.LogInformation("'{ItemName}' build completed", item.Name);
         return result;
     }
 
@@ -54,6 +72,55 @@ public partial class TkProject(string folderPath) : ObservableObject
         using FileStream output = File.Create(projectFilePath);
         JsonSerializer.Serialize(output, Mod);
         
-        // TODO: Save options
+        SaveOptionsGroups();
+    }
+
+    private void SaveOptionsGroups()
+    {
+        foreach (TkModOptionGroup group in Mod.OptionGroups) {
+            if (!TryGetPath(group, out string? groupFolderPath)) {
+                continue;
+            }
+            
+            string metadataFilePath = Path.Combine(groupFolderPath, "info.json");
+            using FileStream fs = File.Create(metadataFilePath);
+            JsonSerializer.Serialize(fs, group);
+
+            SaveOptions(group);
+        }
+    }
+
+    private void SaveOptions(TkModOptionGroup group)
+    {
+        foreach (TkModOption option in group.Options) {
+            if (!TryGetPath(option, out string? optionPath)) {
+                continue;
+            }
+            
+            string metadataFilePath = Path.Combine(optionPath, "info.json");
+            using FileStream fs = File.Create(metadataFilePath);
+            JsonSerializer.Serialize(fs, group);
+        }
+    }
+
+    public void RegisterItem(TkItem option, string sourceFolderPath)
+    {
+        _itemPathLookup[option] = sourceFolderPath;
+    }
+
+    private bool TryGetPath(TkItem item, [MaybeNullWhen(false)] out string path)
+    {
+        if (_itemPathLookup.TryGetValue(item, out string? folderPath) && Directory.Exists(folderPath)) {
+            path = folderPath;
+            return true;
+        }
+        
+        TkLog.Instance.LogWarning("""
+            The source folder for the item '{ItemName}' could not be found.
+            The folder may have been moved or deleted, this item will not be part of the output.
+            """, item.Name);
+
+        path = null;
+        return false;
     }
 }
--- TkSharp.Packaging/TkProjectManager.cs
+++ TkSharp.Packaging/TkProjectManager.cs
@@ -6,8 +6,10 @@ namespace TkSharp.Packaging;
 
 public static class TkProjectManager
 {
+    private static readonly string _storeFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ".tk-studio", "recent.json");
+
     public static ObservableCollection<TkProject> RecentProjects { get; } = [];
-    
+
     public static int MaxRecentProjects { get; set; } = 15;
 
     public static TkProject NewProject(string sourceFolderPath)
@@ -25,6 +27,33 @@ public static class TkProjectManager
         return project;
     }
 
+    public static void Load()
+    {
+        if (!File.Exists(_storeFilePath)) {
+            return;
+        }
+
+        RecentProjects.Clear();
+        using FileStream fs = File.OpenRead(_storeFilePath);
+        List<string> recentProjectFolders = JsonSerializer.Deserialize<List<string>>(fs)
+                                            ?? [];
+
+        foreach (TkProject project in recentProjectFolders.Select(projectFolder => new TkProject(projectFolder))) {
+            LoadProjectMetadataFromFolder(project);
+            RecentProjects.Add(project);
+        }
+    }
+
+    public static void Save()
+    {
+        if (Path.GetDirectoryName(_storeFilePath) is string folder) {
+            Directory.CreateDirectory(folder);
+        }
+        
+        using FileStream fs = File.Create(_storeFilePath);
+        JsonSerializer.Serialize(fs, RecentProjects.Select(proj => proj.FolderPath));
+    }
+
     private static void LoadProjectMetadataFromFolder(TkProject project)
     {
         string projectFilePath = Path.Combine(project.FolderPath, ".tkproj");
@@ -32,7 +61,7 @@ public static class TkProjectManager
             project.Mod = new TkMod {
                 Name = Path.GetFileNameWithoutExtension(projectFilePath)
             };
-            
+
             LoadProjectOptionsFromFolder(project);
             return;
         }
@@ -46,7 +75,59 @@ public static class TkProjectManager
 
     private static void LoadProjectOptionsFromFolder(TkProject project)
     {
-        
+        string optionsFolderPath = Path.Combine(project.FolderPath, "options");
+        if (!Directory.Exists(optionsFolderPath)) {
+            return;
+        }
+
+        foreach (string optionGroupFolderPath in Directory.EnumerateDirectories(optionsFolderPath)) {
+            LoadOptionGroupFolder(project, optionGroupFolderPath);
+        }
+    }
+
+    private static void LoadOptionGroupFolder(TkProject project, string optionGroupFolderPath)
+    {
+        TkModOptionGroup group;
+
+        string metadataFilePath = Path.Combine(optionGroupFolderPath, "info.json");
+        if (File.Exists(metadataFilePath)) {
+            using FileStream fs = File.OpenRead(metadataFilePath);
+            group = JsonSerializer.Deserialize<TkModOptionGroup>(fs)
+                    ?? new TkModOptionGroup();
+            goto CollectOptions;
+        }
+
+        group = new TkModOptionGroup {
+            Name = Path.GetFileNameWithoutExtension(optionGroupFolderPath)
+        };
+
+    CollectOptions:
+        foreach (string optionFolderPath in Directory.EnumerateDirectories(optionGroupFolderPath)) {
+            LoadOptionFolder(project, group, optionFolderPath);
+        }
+
+        project.RegisterItem(group, optionGroupFolderPath);
+        project.Mod.OptionGroups.Add(group);
+    }
+
+    private static void LoadOptionFolder(TkProject project, TkModOptionGroup group, string optionFolderPath)
+    {
+        TkModOption option;
+
+        string metadataFilePath = Path.Combine(optionFolderPath, "info.json");
+        if (File.Exists(metadataFilePath)) {
+            using FileStream fs = File.OpenRead(metadataFilePath);
+            option = JsonSerializer.Deserialize<TkModOption>(fs) ?? new TkModOption();
+            goto Result;
+        }
+
+        option = new TkModOption {
+            Name = Path.GetFileNameWithoutExtension(optionFolderPath)
+        };
+
+    Result:
+        project.RegisterItem(option, optionFolderPath);
+        group.Options.Add(option);
     }
 
     private static void InsertRecentProject(TkProject project)
@@ -54,21 +135,21 @@ public static class TkProjectManager
         if (RecentProjects.Count == 0) {
             goto Default;
         }
-        
+
         for (int i = 0; i < RecentProjects.Count; i++) {
             if (RecentProjects[i].FolderPath != project.FolderPath) {
                 continue;
             }
-            
+
             RecentProjects.RemoveAt(i);
             goto Default;
         }
-        
+
     Default:
         if (RecentProjects.Count > MaxRecentProjects) {
             RecentProjects.RemoveAt(RecentProjects.Count - 1);
         }
-        
+
         RecentProjects.Insert(0, project);
     }
 }
