d1c9e9218288d8320796b733fc53740508bb7372
ArchLeaders <archleadership28@gmail.com>
2024-12-09 02:12:59 -0800

    Initialize TkStoredItem with generated ID

--- TkSharp.Core/Models/TkStoredItem.cs
+++ TkSharp.Core/Models/TkStoredItem.cs
@@ -2,7 +2,7 @@ namespace TkSharp.Core.Models;
 
 public abstract class TkStoredItem : TkItem
 {
-    public Ulid Id { get; init; }
+    public Ulid Id { get; init; } = Ulid.NewUlid();
 
     public TkChangelog Changelog { get; set; } = null!;
 }
