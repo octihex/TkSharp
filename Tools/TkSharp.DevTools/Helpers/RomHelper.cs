728906f822b3b2c1056a45ac3c99d5769fc54dda
ArchLeaders <archleadership28@gmail.com>
2024-12-28 19:29:30 -0800

    Fix RomHelper

--- Tools/TkSharp.DevTools/Helpers/RomHelper.cs
+++ Tools/TkSharp.DevTools/Helpers/RomHelper.cs
@@ -1,3 +1,5 @@
+using LibHac.Common.Keys;
+using Microsoft.Extensions.Logging;
 using TkSharp.Core;
 using TkSharp.Core.IO;
 using TkSharp.Data.Embedded;
@@ -18,11 +20,34 @@ public class RomHelper
         }
 
         if (Config.Shared.KeysFolderPath is string keysFolderPath
+            && GetKeys(keysFolderPath) is KeySet keys 
             && Config.Shared.BaseGameFilePath is string baseGameFilePath
             && Config.Shared.GameUpdateFilePath is string gameUpdateFilePath) {
-            return new PackedTkRom(_checksums, keysFolderPath, baseGameFilePath, gameUpdateFilePath);
+            return new PackedTkRom(_checksums, keys, baseGameFilePath, gameUpdateFilePath);
         }
 
         throw new InvalidOperationException("Invalid configuration.");
+    }
+    
+    public static KeySet? GetKeys(string keysFolder)
+    {
+        string prodKeysFilePath = Path.Combine(keysFolder, "prod.keys");
+        if (!File.Exists(prodKeysFilePath)) {
+            TkLog.Instance.LogError("A 'prod.keys' file could not be found in '{KeysFolder}'", keysFolder);
+            return null;
+        }
+        
+        string titleKeysFilePath = Path.Combine(keysFolder, "title.keys");
+        if (!File.Exists(titleKeysFilePath)) {
+            TkLog.Instance.LogError("A 'title.keys' file could not be found in '{KeysFolder}'", keysFolder);
+            return null;
+        }
+
+        KeySet keys = new();
+        ExternalKeyReader.ReadKeyFile(keys,
+            prodKeysFilename: prodKeysFilePath,
+            titleKeysFilename: titleKeysFilePath);
+
+        return keys;
     }
 }
