1ffc549f1a42553ec52a6811655e6eaca28b409a
ArchLeaders <archleadership28@gmail.com>
2025-03-04 02:45:18 -0800

    Refactor the default byml changelog builder (Hopefully without breaking it)

--- TkSharp.Merging/ChangelogBuilders/BinaryYaml/BymlArrayChangelogBuilder.cs
+++ TkSharp.Merging/ChangelogBuilders/BinaryYaml/BymlArrayChangelogBuilder.cs
@@ -8,54 +8,58 @@ namespace TkSharp.Merging.ChangelogBuilders.BinaryYaml;
 public class BymlArrayChangelogBuilder : IBymlArrayChangelogBuilder
 {
     public static readonly BymlArrayChangelogBuilder Instance = new();
-    
+
     public bool LogChanges(ref BymlTrackingInfo info, ref Byml root, BymlArray src, BymlArray vanilla)
     {
         BymlArrayChangelog changelog = [];
-        List<int> editedVanillaIndices = [];
-        int detectedAdditions = 0;
-        
+        Queue<int> additions = [];
+
         using var vanillaRecordsFound = RentedBitArray.Create(vanilla.Count);
+        using var srcRecordsThatAreVanilla = RentedBitArray.Create(src.Count);
 
         for (int i = 0; i < src.Count; i++) {
             Byml element = src[i];
             if (!vanilla.TryGetIndex(element, Byml.ValueEqualityComparer.Default, out int vanillaIndex)) {
+                additions.Enqueue(i);
                 continue;
             }
 
-            src[i] = BymlChangeType.Remove;
+            // src[i] = BymlChangeType.Remove;
+            srcRecordsThatAreVanilla[i] = true;
             vanillaRecordsFound[vanillaIndex] = true;
         }
 
         for (int i = 0; i < vanilla.Count; i++) {
+            // This vanilla entry has a 1:1 pair in
+            // the modified array, so ignore this step
             if (vanillaRecordsFound[i]) {
                 continue;
             }
 
-            if (i < src.Count) {
-                editedVanillaIndices.Add(i);
+            // If the modded value at this index is
+            // vanilla, remove the vanilla index
+            if (i < src.Count && srcRecordsThatAreVanilla[i] is false) {
+                // If we are still in the bounds of the
+                // modded array, but the value is not vanilla,
+                // consider this index modified
+                Byml element = src[i];
+                BymlChangelogBuilder.LogChangesInline(ref info, ref element, vanilla[i]);
+                changelog.Add(
+                    (index: i, BymlChangeType.Edit, node: element)
+                );
+                additions.Dequeue();
                 continue;
             }
 
-            changelog.Add((i, BymlChangeType.Remove, new Byml()));
+            changelog.Add(
+                (index: i, BymlChangeType.Remove, node: new Byml())
+            );
         }
 
-        for (int i = 0; i < src.Count; i++) {
-            Byml element = src[i];
-            if (element.Type is BymlNodeType.Changelog) {
-                continue;
-            }
-
-            if (editedVanillaIndices.Count > 0) {
-                int vanillaIndex = editedVanillaIndices[0];
-                BymlChangelogBuilder.LogChangesInline(ref info, ref element, vanilla[vanillaIndex]);
-                changelog.Add((vanillaIndex, BymlChangeType.Edit, element));
-                editedVanillaIndices.RemoveAt(0);
-                continue;
-            }
-
-            changelog.Add((i - detectedAdditions, BymlChangeType.Add, element));
-            detectedAdditions++;
+        foreach (int index in additions) {
+            changelog.Add(
+                (index, BymlChangeType.Add, node: src[index])
+            );
         }
 
         root = changelog;
