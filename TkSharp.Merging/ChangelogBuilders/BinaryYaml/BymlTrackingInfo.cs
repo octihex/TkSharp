c7de8d89b698c0cf3841430a38a3b482a5c26e72
ArchLeaders <archleadership28@gmail.com>
2025-03-08 10:56:49 -0800

    Conditionally check byml class Fixes mods with BwavInfo

--- TkSharp.Merging/ChangelogBuilders/BinaryYaml/BymlTrackingInfo.cs
+++ TkSharp.Merging/ChangelogBuilders/BinaryYaml/BymlTrackingInfo.cs
@@ -3,7 +3,9 @@ namespace TkSharp.Merging.ChangelogBuilders.BinaryYaml;
 public ref struct BymlTrackingInfo(ReadOnlySpan<char> canonical, int depth)
 {
     public ReadOnlySpan<char> Type = Path.GetExtension(
-        Path.GetFileNameWithoutExtension(canonical))[1..];
-    
+        Path.GetFileNameWithoutExtension(canonical)) is { Length: > 0 } type
+        ? type[1..]
+        : [];
+
     public int Depth = depth;
 }
